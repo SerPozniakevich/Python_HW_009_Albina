240b3a8a4c01aca5d9a2e76e85a0d6a7b1190119
Sergey Pozniakevich <ven07@yandex.ru>
2022-11-05 09:38:26 +0700

    add main file

---
+++ main.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, executor
+import handlers
+
+API_TOKEN = '5525768983:AAFR4wDOH86vK_RGlgnlNi7EfDdCJV1SAhE'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+dp.register_message_handler(handlers.hi, commands=["hi"])
+dp.register_message_handler(handlers.time, commands=["time"])
+dp.register_message_handler(handlers.help, commands=["help"])
+dp.register_message_handler(handlers.sum, commands=["sum"])
+dp.register_message_handler(handlers.multi, commands=["multi"])
+dp.register_message_handler(handlers.div, commands=["div"])
+dp.register_message_handler(handlers.sub, commands=["sub"])
+
+if __name__ == '__main__':
+
+    print('Бот включён')
+    executor.start_polling(dp, skip_updates=True) # пропуск команд отправленных до старта бота
+    print('Бот выключен')
